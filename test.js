c6c375125b205b2b478c9b33b7ab7aba0d3cee0e
Yikuan Sun <yikuansun2015@gmail.com>
2024-04-02 20:26:12 -0400

    npm init and stuff

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ let pea = new HeadlessPhotopea({ showBrowser: true, });
 console.log(await pea.runScript(`app.echoToOE("hi")`));
 let asset = fs.readFileSync("./coolImage.png");
 console.log(await pea.addBinaryAsset(asset));
-await pea.openFromURL("https://i.imgur.com/wVIP3ow.png", false);
+await pea.openFromURL("https://i.imgur.com/wVIP3ow.png", true);
 await pea.runScript("app.activeDocument.activeLayer.blendMode = 'scrn'");
